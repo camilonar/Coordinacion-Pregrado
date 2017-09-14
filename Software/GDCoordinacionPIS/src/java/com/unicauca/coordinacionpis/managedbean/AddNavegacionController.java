867b608f609949efaea6628f51542a8c930e8862
solartes <juliansolarter@gmail.com>
2017-09-13 23:33:21 -0500

    Se agrega miga de pan

--- Software/GDCoordinacionPIS/src/java/com/unicauca/coordinacionpis/managedbean/AddNavegacionController.java
+++ Software/GDCoordinacionPIS/src/java/com/unicauca/coordinacionpis/managedbean/AddNavegacionController.java
@@ -22,132 +22,153 @@ import org.primefaces.model.menu.MenuModel;
 @SessionScoped
 public class AddNavegacionController implements Serializable {
 
-    private MenuModel menuNavegacion = new DefaultMenuModel();
+    private MenuModel menuNavegacion;
+    DefaultMenuItem index;
+    DefaultMenuItem miga1;
+    DefaultMenuItem miga2;
 
     public MenuModel getMenu() {
         return menuNavegacion;
     }
 
     public AddNavegacionController() {
-        this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
-        index.setValue("Index");
+        menuNavegacion = new DefaultMenuModel();
+        index = new DefaultMenuItem();
+        index.setStyle("cursor: default;text-decoration: none;");
+        index.setValue("Gestión de departamentos o facultades");
+        miga1 = new DefaultMenuItem();
+        miga1.setStyle("cursor: default;text-decoration: none;");
+        miga1.setValue("Gestión de departamentos o facultades");
+        miga2 = new DefaultMenuItem();
+        miga2.setStyle("cursor: default;text-decoration: none;");
+        miga2.setValue("Gestión de departamentos o facultades");
+    }
 
-        DefaultMenuItem inicio = new DefaultMenuItem();
-        inicio.setValue("Inicio");
+    public MenuModel migaPrincipal(String tipo) {
+        if (tipo.equals("1")) {
+            this.menuNavegacion = new DefaultMenuModel();
+            index.setValue("Index");
+            miga1.setValue("Usuarios");
+            miga2.setValue("Gestión de usuarios");
+
+            this.menuNavegacion.addElement(index);
+            this.menuNavegacion.addElement(miga1);
+            this.menuNavegacion.addElement(miga2);
+        }
+        if (tipo.equals("2")) {
+            this.menuNavegacion = new DefaultMenuModel();
+            index.setValue("Index");
+            miga1.setValue("Planes de estudio");
+            miga2.setValue("Gestión planes de estudio");
+
+            this.menuNavegacion.addElement(index);
+            this.menuNavegacion.addElement(miga1);
+            this.menuNavegacion.addElement(miga2);
+        }
+
+        if (tipo.equals("3")) {
+            this.menuNavegacion = new DefaultMenuModel();
+            index.setValue("Index");
+            miga1.setValue("Oferta académica");
+            miga2.setValue("Listado de ofertas académicas");
+
+            this.menuNavegacion.addElement(index);
+            this.menuNavegacion.addElement(miga1);
+            this.menuNavegacion.addElement(miga2);
+        }
+
+        return menuNavegacion;
+    }
+
+    public void addGestionUsuario() {
+        this.menuNavegacion = new DefaultMenuModel();
+        index.setValue("Index");
+        miga1.setValue("Usuarios");
+        miga2.setValue("Gestión de usuarios");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(inicio);
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
     }
 
     public void addOfertaAcademicaGestion() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
-
-        DefaultMenuItem ofertaAcademica = new DefaultMenuItem();
-        ofertaAcademica.setValue("Oferta académica");
-
-        DefaultMenuItem ofertaAcademicaGestion = new DefaultMenuItem();
-        ofertaAcademicaGestion.setValue("Gestión de ofertas académicas");
+        miga1.setValue("Oferta académica");
+        miga2.setValue("Gestión de ofertas académicas");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(ofertaAcademica);
-        this.menuNavegacion.addElement(ofertaAcademicaGestion);
-
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
     }
 
     public void addOfertaAcademicaLista() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
-
-        DefaultMenuItem ofertaAcademica = new DefaultMenuItem();
-        ofertaAcademica.setValue("Oferta académica");
-
-        DefaultMenuItem ofertaAcademicaGestion = new DefaultMenuItem();
-        ofertaAcademicaGestion.setValue("Listado de ofertas académicas");
+        miga1.setValue("Oferta académica");
+        miga2.setValue("Listado de ofertas académicas");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(ofertaAcademica);
-        this.menuNavegacion.addElement(ofertaAcademicaGestion);
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
 
     }
 
     public void addOfertaAcademicaRegistroMaterias() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
-
-        DefaultMenuItem ofertaAcademica = new DefaultMenuItem();
-        ofertaAcademica.setValue("Oferta académica");
-
-        DefaultMenuItem ofertaAcademicaGestion = new DefaultMenuItem();
-        ofertaAcademicaGestion.setValue("Registrar materia");
+        miga1.setValue("Materias");
+        miga2.setValue("Gestión de materias");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(ofertaAcademica);
-        this.menuNavegacion.addElement(ofertaAcademicaGestion);
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
 
     }
 
     public void addOfertaAcademicaRegistroDepFac() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
-
-        DefaultMenuItem ofertaAcademica = new DefaultMenuItem();
-        ofertaAcademica.setValue("Oferta académica");
-
-        DefaultMenuItem ofertaAcademicaGestion = new DefaultMenuItem();
-        ofertaAcademicaGestion.setValue("Registrar departamento o facultad");
+        miga1.setValue("Departamentos y facultades");
+        miga2.setValue("Gestión de departamentos o facultades");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(ofertaAcademica);
-        this.menuNavegacion.addElement(ofertaAcademicaGestion);
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
 
     }
 
     public void addPlanesDeEstudioGestion() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
-
-        DefaultMenuItem planesDeEstudio = new DefaultMenuItem();
-        planesDeEstudio.setValue("Planes de estudio");
-
-        DefaultMenuItem planesDeEstudioGestion = new DefaultMenuItem();
-        planesDeEstudioGestion.setValue("Gestión planes de estudio");
+        miga1.setValue("Planes de estudio");
+        miga2.setValue("Gestión planes de estudio");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(planesDeEstudio);
-        this.menuNavegacion.addElement(planesDeEstudioGestion);
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
 
     }
 
     public void addAnteproyectoGestion() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
-
-        DefaultMenuItem anteproyecto = new DefaultMenuItem();
-        anteproyecto.setValue("Anteproyecto");
-
-        DefaultMenuItem anteproyectoGestion = new DefaultMenuItem();
-        anteproyectoGestion.setValue("Gestión de anteproyectos");
+        miga1.setValue("Anteproyecto");
+        miga2.setValue("Gestión de anteproyectos");
 
         this.menuNavegacion.addElement(index);
-        this.menuNavegacion.addElement(anteproyecto);
-        this.menuNavegacion.addElement(anteproyectoGestion);
+        this.menuNavegacion.addElement(miga1);
+        this.menuNavegacion.addElement(miga2);
 
     }
 
     public void cambioDeOpciones() {
         this.menuNavegacion = new DefaultMenuModel();
-        DefaultMenuItem index = new DefaultMenuItem();
         index.setValue("Index");
 
         DefaultMenuItem inicio = new DefaultMenuItem();
+        inicio.setStyle("cursor: default;text-decoration: none;");
+        inicio.setValue("Gestión de departamentos o facultades");
         inicio.setValue("Inicio");
 
         this.menuNavegacion.addElement(index);
