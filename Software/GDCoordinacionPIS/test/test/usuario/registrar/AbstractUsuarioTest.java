091cbe52492637db5deb5f64aef816e8d1d224e6
Camilo <camilonar09@gmail.com>
2017-09-23 17:20:52 -0500

    Casos de prueba del registro de usuario
    Pruebas con JWebUnit para las validaciones del registro de usuario

---
+++ Software/GDCoordinacionPIS/test/test/usuario/registrar/AbstractUsuarioTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.usuario.registrar;
+
+import net.sourceforge.jwebunit.junit.JWebUnit;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertButtonPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickButton;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+
+/**
+ *
+ * @author Camilo
+ */
+public class AbstractUsuarioTest {
+    
+    //Configuración
+    private static String nombreUsuario = "admin";
+    private static String contrasena = "jefe";
+    
+    protected void goToUsuario(){
+        beginAt("/GDCP/sinSesion/index.xhtml"); 
+        JWebUnit.setTextField("form-inicio:nombreUsuario", nombreUsuario);
+        JWebUnit.setTextField("form-inicio:contrasena", contrasena);
+        assertButtonPresent("form-inicio:login");
+        clickButton("form-inicio:login");
+        assertLinkPresent("menu-admin:menuitem-usuarios");
+        clickLink("menu-admin:menuitem-usuarios");
+    }
+    
+    protected void goToRegistrar(){
+        goToUsuario();
+        JWebUnit.assertMatch("true",JWebUnit.getElementById("UsuarioCreateDlg").getAttribute("aria-hidden"));
+        assertButtonPresent("form:registrar-usuario");
+        clickButton("form:registrar-usuario");
+        JWebUnit.assertMatch("false", JWebUnit.getElementById("UsuarioCreateDlg").getAttribute("aria-hidden"));
+    }
+}
