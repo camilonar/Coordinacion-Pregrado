d1ec72d2618840466aaf4428e5a4ea9943d5c149
DanielaVelasquez <angiedanielav@unicauca.edu.co>
2017-11-20 21:37:57 -0500

    Corrección script

--- Diagramas/correciones-noviembre-20.sql
+++ Diagramas/correciones-noviembre-20.sql
@@ -4,4 +4,4 @@ ALTER TABLE profesor ADD COLUMN codigoProfesor VARCHAR(15) NOT NULL;
 ALTER TABLE estudiante DROP COLUMN codigoEstudiante;
 ALTER TABLE estudiante ADD COLUMN codigoEstudiante VARCHAR(15) NOT NULL;
 
-ALTER TABLE anteproyecto MODIFY COLUMN tituloAnteproyecto VARCHAR(100) NOT NULL;
+ALTER TABLE anteproyecto MODIFY COLUMN tituloAnteproyecto VARCHAR(300) NOT NULL;
