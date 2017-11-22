d7bde6291d28d67e33c602ff64b6de0b08ac4c86
Pacavi <pacavi007@unicauca.edu.co>
2017-11-22 12:48:50 -0500

    Manuales Segun Pantoja Actualizados

--- Diagramas/exec-2.sql
+++
@@ -1,4 +0,0 @@
---Ejecutar este script si se ha ejecutado un script sobre bd antes del 08 de octubre 
-ALTER TABLE programa DROP FOREIGN KEY fk_programa_facultad1;
-ALTER TABLE programa DROP COLUMN idfacultad;
-DROP TABLE facultad;
